ccde7267ac11660fed133a0d3fe4053ba507b492
Saurav <sauravpradhanang001@gmail.com>
2025-02-28 22:08:45 +0545

    modified vehicle data

--- src/traffic_simulation.h
+++ src/traffic_simulation.h
@@ -1,1 +1,44 @@
 #include <stdio.h>
+#ifndef TRAFFIC_SIMULATION_H
+#define TRAFFIC_SIMULATION_H
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+// Enum for directions
+typedef enum {
+    LEFT = 0,
+    RIGHT = 1,
+    TOP = 2,
+    BOTTOM = 3
+} Direction;
+
+// Vehicle struct declaration
+typedef struct {
+    SDL_Rect rect;
+    bool active;
+    int speed;
+    // SDL_Color color;
+    Direction direction;
+} Vehicle;
+
+// TrafficLight struct declaration
+typedef struct {
+    SDL_Rect rect;
+    bool isGreen;
+} TrafficLight;
+
+// Function prototypes
+void createVehicle();
+void updateVehicles();
+void drawTrafficLightIndicator(SDL_Renderer *renderer);
+void updateTrafficLight();
+
+#define MAX_VEHICLES 100
+extern Vehicle vehicles[MAX_VEHICLES];
+extern TrafficLight trafficLights[4];
+extern int currentLight; // Track which traffic light is green
+
+#endif // TRAFFIC_SIMULATION_H
