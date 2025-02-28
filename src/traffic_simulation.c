59163ab3546089ddffa46399881be597aa13243f
Saurav <sauravpradhanang001@gmail.com>
2025-02-28 22:14:16 +0545

    traffic sim complete

--- src/traffic_simulation.c
+++ src/traffic_simulation.c
@@ -64,3 +64,53 @@ void createVehicle() {
         }
     }
 }
+
+void updateVehicles() {
+    for (int i = 0; i < MAX_VEHICLES; i++) {
+        if (vehicles[i].active) {
+            bool shouldStop = false;
+            
+            if (vehicles[i].direction == LEFT && !trafficLights[0].isGreen && vehicles[i].rect.x < SCREEN_WIDTH * 0.4f) shouldStop = true;
+            if (vehicles[i].direction == RIGHT && !trafficLights[1].isGreen && vehicles[i].rect.x > SCREEN_WIDTH * 0.6f) shouldStop = true;
+            if (vehicles[i].direction == TOP && !trafficLights[2].isGreen && vehicles[i].rect.y < SCREEN_HEIGHT * 0.4f) shouldStop = true;
+            if (vehicles[i].direction == BOTTOM && !trafficLights[3].isGreen && vehicles[i].rect.y > SCREEN_HEIGHT * 0.6f) shouldStop = true;
+
+            if (!shouldStop) {
+                if (vehicles[i].direction == LEFT || vehicles[i].direction == RIGHT) {
+                    vehicles[i].rect.x += vehicles[i].speed;
+                } else {
+                    vehicles[i].rect.y += vehicles[i].speed;
+                }
+            }
+            
+            if (vehicles[i].rect.x > SCREEN_WIDTH || vehicles[i].rect.x < -vehicles[i].rect.w ||
+                vehicles[i].rect.y > SCREEN_HEIGHT || vehicles[i].rect.y < -vehicles[i].rect.h) {
+                vehicles[i].active = false;
+            }
+        }
+    }
+}
+
+void drawTrafficLightIndicator(SDL_Renderer *renderer) {
+    for (int i = 0; i < 4; i++) {
+        SDL_SetRenderDrawColor(renderer, 169, 169, 169, 255);
+        SDL_RenderFillRect(renderer, &trafficLights[i].rect);
+        
+        SDL_Color lightColor = trafficLights[i].isGreen ? (SDL_Color){0, 255, 0, 255} : (SDL_Color){255, 0, 0, 255};
+        SDL_SetRenderDrawColor(renderer, lightColor.r, lightColor.g, lightColor.b, 255);
+        SDL_Rect lightRect = {trafficLights[i].rect.x + 15, trafficLights[i].rect.y + 25, 20, 20};
+        SDL_RenderFillRect(renderer, &lightRect);
+    }
+}
+
+void updateTrafficLight() {
+    static int frameCounter = 0;
+    frameCounter++;
+
+    if (frameCounter % 300 == 0) {
+        trafficLights[currentLight].isGreen = false;
+        currentLight = (currentLight + 1) % 4;
+        trafficLights[currentLight].isGreen = true;
+        frameCounter = 0;
+    }
+}
